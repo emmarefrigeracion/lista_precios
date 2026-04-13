1ad595dae503295c1afa686bca01b745680e577e
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-13 08:55:16 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -12,7 +12,7 @@
   {"Codigo": "agente", "Producto": "AGENTE DE LIMPIEZA BELNET 750ML", "Precio_Cliente": 35820.00, "Precio_Tecnico": 32400.00, "Cantidad_Stock": 4},
   {"Codigo": "5-1-12365", "Producto": "AGENTE LIMPIEZA CLEANER 11 PRO X 1LTS - REFRIOIL (NARANJA)", "Precio_Cliente": 26000.00, "Precio_Tecnico": 23400.00, "Cantidad_Stock": 4},
   {"Codigo": "agi001", "Producto": "AGITADOR DREAN BLUE LARGO", "Precio_Cliente": 3700.00, "Precio_Tecnico": 2700.00, "Cantidad_Stock": 6},
-  {"Codigo": "agi002", "Producto": "AGITADOR DREAN CORTO", "Precio_Cliente": 2970.00, "Precio_Tecnico": 2475.00, "Cantidad_Stock": 7},
+  {"Codigo": "agi002", "Producto": "AGITADOR DREAN CORTO", "Precio_Cliente": 3000.00, "Precio_Tecnico": 2500.00, "Cantidad_Stock": 7},
   {"Codigo": "8-8-13105", "Producto": "AISLACION  1/2\" POR  TIRA", "Precio_Cliente": 2871.00, "Precio_Tecnico": 2277.00, "Cantidad_Stock": 129},
   {"Codigo": "8-9-CO-D19", "Producto": "AISLACION 1/4 POR TIRA", "Precio_Cliente": 2079.00, "Precio_Tecnico": 1386.00, "Cantidad_Stock": 120},
   {"Codigo": "8-9-CO-F35", "Producto": "AISLACION 3/8  POR TIRA", "Precio_Cliente": 2574.00, "Precio_Tecnico": 1881.00, "Cantidad_Stock": 135},
