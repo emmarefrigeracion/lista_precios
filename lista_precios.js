586c4c9cee0e37f1b00c491e58759f0d69c4ea5c
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-14 12:01:06 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -437,7 +437,7 @@
   {"Codigo": "13-1-3012", "Producto": "TAPA SECARROPA", "Precio_Cliente": 11880.00, "Precio_Tecnico": 6930.00, "Cantidad_Stock": 3},
   {"Codigo": "TAP001", "Producto": "TAPAFUGAS EXTREME  12 ML", "Precio_Cliente": 29601.00, "Precio_Tecnico": 24354.00, "Cantidad_Stock": 8},
   {"Codigo": "13646", "Producto": "TEMSOTATO PARA CONGELADOR", "Precio_Cliente": 17424.00, "Precio_Tecnico": 14355.00, "Cantidad_Stock": 2},
-  {"Codigo": "ten5", "Producto": "TENSOR SECARROPA ALLADIO DREAN WIND", "Precio_Cliente": 3400.00, "Precio_Tecnico": 2300.00, "Cantidad_Stock": 6},
+  {"Codigo": "ten5", "Producto": "TENSOR SECARROPA ALLADIO DREAN WIND", "Precio_Cliente": 3400.00, "Precio_Tecnico": 2500.00, "Cantidad_Stock": 6},
   {"Codigo": "sec003", "Producto": "TENSOR SECARROPA KOHINOOR CUADRADO", "Precio_Cliente": 3465.00, "Precio_Tecnico": 2079.00, "Cantidad_Stock": 12},
   {"Codigo": "sec004", "Producto": "TENSOR SECARROPA MOD VIEJO CODINI", "Precio_Cliente": 2100.00, "Precio_Tecnico": 1300.00, "Cantidad_Stock": 7},
   {"Codigo": "ten002", "Producto": "TENSORES PARA LAVARROPA", "Precio_Cliente": 2178.00, "Precio_Tecnico": 1683.00, "Cantidad_Stock": 0},
