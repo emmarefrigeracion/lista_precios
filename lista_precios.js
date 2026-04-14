842020c11fd8efd741044282a3815e0334d407c7
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-14 17:40:49 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -131,7 +131,7 @@
   {"Codigo": "COP003", "Producto": "COPA 60MM", "Precio_Cliente": 23000.00, "Precio_Tecnico": 19000.00, "Cantidad_Stock": 1},
   {"Codigo": "COR018", "Producto": "CORREA 018", "Precio_Cliente": 5500.00, "Precio_Tecnico": 4300.00, "Cantidad_Stock": 5},
   {"Codigo": "cor025", "Producto": "CORREA 019", "Precio_Cliente": 7000.00, "Precio_Tecnico": 5400.00, "Cantidad_Stock": 5},
-  {"Codigo": "COR020", "Producto": "CORREA 020", "Precio_Cliente": 7128.00, "Precio_Tecnico": 5841.00, "Cantidad_Stock": 5},
+  {"Codigo": "COR020", "Producto": "CORREA 020", "Precio_Cliente": 7200.00, "Precio_Tecnico": 5800.00, "Cantidad_Stock": 4},
   {"Codigo": "cor 021", "Producto": "CORREA 021", "Precio_Cliente": 7029.00, "Precio_Tecnico": 5346.00, "Cantidad_Stock": 2},
   {"Codigo": "COR022", "Producto": "CORREA 022", "Precio_Cliente": 5445.00, "Precio_Tecnico": 3960.00, "Cantidad_Stock": 2},
   {"Codigo": "COR73", "Producto": "CORREA 1173", "Precio_Cliente": 17424.00, "Precio_Tecnico": 15147.00, "Cantidad_Stock": 6},
