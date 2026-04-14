50d8642f839f71a270eefb967efa695c0524254c
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-14 09:48:15 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -182,7 +182,7 @@
   {"Codigo": "fil010", "Producto": "FILTRO 1/4 HELADERA COMERCIAL (NARANJA)", "Precio_Cliente": 17200.00, "Precio_Tecnico": 14200.00, "Cantidad_Stock": 2},
   {"Codigo": "FIL001", "Producto": "FILTRO 10 GR S CHICOTE", "Precio_Cliente": 3100.00, "Precio_Tecnico": 2500.00, "Cantidad_Stock": 10},
   {"Codigo": "4-10-014820", "Producto": "FILTRO 10 GRS  C/CHICOTE", "Precio_Cliente": 4200.00, "Precio_Tecnico": 3500.00, "Cantidad_Stock": 10},
-  {"Codigo": "FIL002", "Producto": "FILTRO 15GR S/CHIICOTE", "Precio_Cliente": 4455.00, "Precio_Tecnico": 3861.00, "Cantidad_Stock": 31},
+  {"Codigo": "FIL002", "Producto": "FILTRO 15GR S/CHIICOTE", "Precio_Cliente": 4900.00, "Precio_Tecnico": 3900.00, "Cantidad_Stock": 10},
   {"Codigo": "4-10-014823", "Producto": "FILTRO 15GRS. C/CHICOTE", "Precio_Cliente": 18414.00, "Precio_Tecnico": 13810.50, "Cantidad_Stock": 4},
   {"Codigo": "4-10-014802", "Producto": "FILTRO 15GRS. SIN CHICOTE", "Precio_Cliente": 3168.00, "Precio_Tecnico": 2673.00, "Cantidad_Stock": 11},
   {"Codigo": "4-10-014824", "Producto": "FILTRO 20 GRM  C/CHICOTE", "Precio_Cliente": 4257.00, "Precio_Tecnico": 3762.00, "Cantidad_Stock": 3},
