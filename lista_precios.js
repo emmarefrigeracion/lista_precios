5048ce509a80b4ca219b86cf95fee9e6643edf1e
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-14 09:45:38 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -179,7 +179,7 @@
   {"Codigo": "est002", "Producto": "ESTUFA INFRAROJA FIJA", "Precio_Cliente": 33053.13, "Precio_Tecnico": 0.00, "Cantidad_Stock": 1},
   {"Codigo": "1-16-021113", "Producto": "FERODO CUADRADO CANDY-DREAN", "Precio_Cliente": 8167.50, "Precio_Tecnico": 6831.00, "Cantidad_Stock": 2},
   {"Codigo": "fil035", "Producto": "FILTRO  (NARANJA)  3/8", "Precio_Cliente": 31500.00, "Precio_Tecnico": 27900.00, "Cantidad_Stock": 4},
-  {"Codigo": "fil010", "Producto": "FILTRO 1/4 HELADERA COMERCIAL (NARANJA)", "Precio_Cliente": 16830.00, "Precio_Tecnico": 13959.00, "Cantidad_Stock": 2},
+  {"Codigo": "fil010", "Producto": "FILTRO 1/4 HELADERA COMERCIAL (NARANJA)", "Precio_Cliente": 17200.00, "Precio_Tecnico": 14200.00, "Cantidad_Stock": 2},
   {"Codigo": "FIL001", "Producto": "FILTRO 10 GR S CHICOTE", "Precio_Cliente": 2970.00, "Precio_Tecnico": 2475.00, "Cantidad_Stock": 10},
   {"Codigo": "4-10-014820", "Producto": "FILTRO 10 GRS  C/CHICOTE", "Precio_Cliente": 3820.90, "Precio_Tecnico": 3465.00, "Cantidad_Stock": 9},
   {"Codigo": "FIL002", "Producto": "FILTRO 15GR S/CHIICOTE", "Precio_Cliente": 4455.00, "Precio_Tecnico": 3861.00, "Cantidad_Stock": 31},
