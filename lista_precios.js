f5b7606291b3ac650860fa84b4b7cb754478b9ac
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-14 09:51:38 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -188,7 +188,6 @@
   {"Codigo": "fil020", "Producto": "FILTRO 20G  S/ CHICOTE", "Precio_Cliente": 4200.00, "Precio_Tecnico": 3000.00, "Cantidad_Stock": 8},
   {"Codigo": "4-10-070990", "Producto": "FILTRO 30 GRS S/CHICOTE", "Precio_Cliente": 6200.00, "Precio_Tecnico": 4900.00, "Cantidad_Stock": 10},
   {"Codigo": "fil1", "Producto": "FILTRO 30GRM CON CHICOTE", "Precio_Cliente": 7200.00, "Precio_Tecnico": 5900.00, "Cantidad_Stock": 2},
-  {"Codigo": "fil5", "Producto": "FILTRO DE 1/4 CON TUERCA (NARANJA)", "Precio_Cliente": 17820.00, "Precio_Tecnico": 14850.00, "Cantidad_Stock": 0},
   {"Codigo": "filtro002", "Producto": "FILTRO DE BOMBA LAVARROPA DREAN 166 LARGO", "Precio_Cliente": 16731.00, "Precio_Tecnico": 12870.00, "Cantidad_Stock": 4},
   {"Codigo": "filtro003", "Producto": "FILTRO DE BOMBA LAVARROPA DREAN CORTO", "Precio_Cliente": 7227.00, "Precio_Tecnico": 4851.00, "Cantidad_Stock": 0},
   {"Codigo": "for040", "Producto": "FORZADOR CONDENSADORA SPLIT 33W", "Precio_Cliente": 82071.00, "Precio_Tecnico": 65637.00, "Cantidad_Stock": 0},
