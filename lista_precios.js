266ea8b90ebee805e3e21897aa3eedcfba5a8927
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-14 17:17:43 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -93,7 +93,7 @@
   {"Codigo": "cap2.5", "Producto": "CAPACITOR 2.5", "Precio_Cliente": 3600.00, "Precio_Tecnico": 2600.00, "Cantidad_Stock": 4},
   {"Codigo": "cap4", "Producto": "CAPACITOR 4MF", "Precio_Cliente": 3300.00, "Precio_Tecnico": 2300.00, "Cantidad_Stock": 0},
   {"Codigo": "cap017", "Producto": "CAPACITOR ANTIPARACITARIO  DREAN", "Precio_Cliente": 16700.00, "Precio_Tecnico": 12500.00, "Cantidad_Stock": 0},
-  {"Codigo": "4-21-01823", "Producto": "CAPACITOR COOLTECH 30MF 440V", "Precio_Cliente": 6900.00, "Precio_Tecnico": 5800.00, "Cantidad_Stock": 0},
+  {"Codigo": "capa30", "Producto": "CAPACITOR COOLTECH 30MF 440V", "Precio_Cliente": 6900.00, "Precio_Tecnico": 5800.00, "Cantidad_Stock": 0},
   {"Codigo": "4-22-2172", "Producto": "CAPACITOR COOLTECH 35 MF", "Precio_Cliente": 7900.00, "Precio_Tecnico": 6500.00, "Cantidad_Stock": 4},
   {"Codigo": "4-21-44040", "Producto": "CAPACITOR COOLTECH 40MF", "Precio_Cliente": 9600.00, "Precio_Tecnico": 8000.00, "Cantidad_Stock": 0},
   {"Codigo": "4-21-010828", "Producto": "CAPACITOR COOLTECH 50MF 440V", "Precio_Cliente": 9900.00, "Precio_Tecnico": 8600.00, "Cantidad_Stock": 4},
