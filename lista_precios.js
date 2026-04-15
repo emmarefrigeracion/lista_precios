f8e00889fa75677abace1f0d0fdd920b00c0abf3
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-15 10:56:10 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -299,7 +299,7 @@
   {"Codigo": "pa1", "Producto": "PATAS LAVARROPAS ORIGINALES DREAN  PRECIO POR UNIDAD", "Precio_Cliente": 5346.00, "Precio_Tecnico": 4257.00, "Cantidad_Stock": 2},
   {"Codigo": "per005", "Producto": "PERILLA  LAV DREAN NEXT", "Precio_Cliente": 9600.00, "Precio_Tecnico": 7800.00, "Cantidad_Stock": 2},
   {"Codigo": "per2", "Producto": "PERILLA BLUE/GOLD", "Precio_Cliente": 5600.00, "Precio_Tecnico": 3600.00, "Cantidad_Stock": 2},
-  {"Codigo": "TER009", "Producto": "PERILLA PARA TERMOSTATO", "Precio_Cliente": 2475.00, "Precio_Tecnico": 1683.00, "Cantidad_Stock": 2},
+  {"Codigo": "ter19", "Producto": "PERILLA PARA TERMOSTATO", "Precio_Cliente": 2900.00, "Precio_Tecnico": 1900.00, "Cantidad_Stock": 2},
   {"Codigo": "pe021", "Producto": "PERILLA SECARROPA DREAN (AZULES)", "Precio_Cliente": 5200.00, "Precio_Tecnico": 3900.00, "Cantidad_Stock": 4},
   {"Codigo": "per002", "Producto": "PERSOSTATO 166 BLUE ORIGINAL", "Precio_Cliente": 0.00, "Precio_Tecnico": 12375.00, "Cantidad_Stock": 0},
   {"Codigo": "per001", "Producto": "PERSOSTATO BLUE FRONTAL", "Precio_Cliente": 0.00, "Precio_Tecnico": 19503.00, "Cantidad_Stock": 0},
