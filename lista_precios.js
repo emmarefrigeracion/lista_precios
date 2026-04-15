db48a25a395a872ae7e5e750562bb3d6f5f5b712
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-15 11:37:41 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -17,7 +17,7 @@
   {"Codigo": "8-9-CO-D19", "Producto": "AISLACION 1/4 POR TIRA", "Precio_Cliente": 2500.00, "Precio_Tecnico": 1500.00, "Cantidad_Stock": 100},
   {"Codigo": "8-9-CO-F35", "Producto": "AISLACION 3/8  POR TIRA", "Precio_Cliente": 2600.00, "Precio_Tecnico": 1900.00, "Cantidad_Stock": 1400},
   {"Codigo": "8-8-13139", "Producto": "AISLACION 5/8\" NEGROS POR TIRA", "Precio_Cliente": 3600.00, "Precio_Tecnico": 2500.00, "Cantidad_Stock": 100},
-  {"Codigo": "am1", "Producto": "AMORTIGUADOR DREAN 6-8 KG PRECIO POR UNIDAD", "Precio_Cliente": 12000.00, "Precio_Tecnico": 8900.00, "Cantidad_Stock": 12},
+  {"Codigo": "am1", "Producto": "AMORTIGUADOR DREAN 6-8 KG PRECIO POR UNIDAD", "Precio_Cliente": 12000.00, "Precio_Tecnico": 8500.00, "Cantidad_Stock": 12},
   {"Codigo": "amo004", "Producto": "AMORTIGUADORES (CONJ SUSP)   CONSUL", "Precio_Cliente": 62766.00, "Precio_Tecnico": 57618.00, "Cantidad_Stock": 1},
   {"Codigo": "amo4", "Producto": "AMORTIGUADORES (CONJ SUSP) CONCEPT MOD NUEVO", "Precio_Cliente": 52100.00, "Precio_Tecnico": 46800.00, "Cantidad_Stock": 2},
   {"Codigo": "amo003", "Producto": "AMORTIGUADORES CANDI / LONGVIE POR UNIDAD", "Precio_Cliente": 16000.00, "Precio_Tecnico": 13000.00, "Cantidad_Stock": 2},
