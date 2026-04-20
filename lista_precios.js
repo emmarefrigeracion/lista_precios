050f1b40f0620894b2f1612ce28f6ad6e9c10cff
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-20 18:55:26 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -352,7 +352,7 @@
   {"Codigo": "ret015", "Producto": "RETEN 25*50*10 VUPRE ESPAÑA", "Precio_Cliente": 0.00, "Precio_Tecnico": 0.00, "Cantidad_Stock": 6},
   {"Codigo": "1-34-0523106", "Producto": "RETEN 25X47X10", "Precio_Cliente": 2574.00, "Precio_Tecnico": 1683.00, "Cantidad_Stock": 0},
   {"Codigo": "retensav", "Producto": "RETEN 25X47X10 SAV", "Precio_Cliente": 3000.00, "Precio_Tecnico": 2300.00, "Cantidad_Stock": 18},
-  {"Codigo": "1-34-1026227", "Producto": "RETEN 30X52X10", "Precio_Cliente": 3000.00, "Precio_Tecnico": 2400.00, "Cantidad_Stock": 38},
+  {"Codigo": "1-34-1026227", "Producto": "RETEN 30X52X10  SAV", "Precio_Cliente": 3000.00, "Precio_Tecnico": 2400.00, "Cantidad_Stock": 38},
   {"Codigo": "ret001", "Producto": "RETEN 30X64X17 CONSUL", "Precio_Cliente": 6100.00, "Precio_Tecnico": 4500.00, "Cantidad_Stock": 0},
   {"Codigo": "retconc", "Producto": "RETEN CONCEPT ORIG 25/55/10", "Precio_Cliente": 8900.00, "Precio_Tecnico": 7500.00, "Cantidad_Stock": 4},
   {"Codigo": "1-38-11286", "Producto": "RETEN LG 35X66X10 LAVARROPA", "Precio_Cliente": 6435.00, "Precio_Tecnico": 4752.00, "Cantidad_Stock": 0},
