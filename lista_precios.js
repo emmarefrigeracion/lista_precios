ecadeef0a7a08d7fc2d9a20a616b1032ba19c5c3
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-21 10:38:25 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -164,7 +164,7 @@
   {"Codigo": "do001", "Producto": "DOBLADORA DE CAÑO 3 EN 1", "Precio_Cliente": 19701.00, "Precio_Tecnico": 15444.00, "Cantidad_Stock": 0},
   {"Codigo": "res004", "Producto": "DOBLADORA RESORTE 5/8", "Precio_Cliente": 6633.00, "Precio_Tecnico": 5742.00, "Cantidad_Stock": 1},
   {"Codigo": "d2", "Producto": "DUCTO SALIDA DE AIRE PORTATIL 5\" X 2 METROS", "Precio_Cliente": 23760.00, "Precio_Tecnico": 19305.00, "Cantidad_Stock": 0},
-  {"Codigo": "ele5", "Producto": "ELECTROVALVULA 1 VIA 180º /90º", "Precio_Cliente": 11880.00, "Precio_Tecnico": 8613.00, "Cantidad_Stock": 5},
+  {"Codigo": "ele5", "Producto": "ELECTROVALVULA 1 VIA 180º /90º", "Precio_Cliente": 11900.00, "Precio_Tecnico": 8700.00, "Cantidad_Stock": 9},
   {"Codigo": "1-2-020703", "Producto": "ELECTROVALVULA 2 VIAS 180º", "Precio_Cliente": 16200.00, "Precio_Tecnico": 13500.00, "Cantidad_Stock": 1},
   {"Codigo": "1-2-2760013", "Producto": "ELECTROVALVULA 2 VIAS 90º", "Precio_Cliente": 16204.32, "Precio_Tecnico": 13442.22, "Cantidad_Stock": 2},
   {"Codigo": "ele005", "Producto": "ELECTROVALVULA FICHA DREAN BLUE ORIGINAL", "Precio_Cliente": 19000.00, "Precio_Tecnico": 16900.00, "Cantidad_Stock": 5},
