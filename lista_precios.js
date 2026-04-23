a0fb9a9f88caad0bb4088c25adf42eda5506cde3
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-23 11:38:26 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -211,7 +211,7 @@
   {"Codigo": "GAS006", "Producto": "GAS R134 ANTON / FREON  * 900 GRM.", "Precio_Cliente": 31000.00, "Precio_Tecnico": 27500.00, "Cantidad_Stock": 6},
   {"Codigo": "gas020", "Producto": "GAS R134 TORRINGTON", "Precio_Cliente": 27819.00, "Precio_Tecnico": 21483.00, "Cantidad_Stock": 0},
   {"Codigo": "gas023", "Producto": "GAS R22 1KG FINLEY", "Precio_Cliente": 39000.00, "Precio_Tecnico": 36600.00, "Cantidad_Stock": 2},
-  {"Codigo": "GAS007", "Producto": "GAS R22 X 1KG FREON", "Precio_Cliente": 45000.00, "Precio_Tecnico": 40000.00, "Cantidad_Stock": 1},
+  {"Codigo": "GAS007", "Producto": "GAS R22 X 1KG FREON", "Precio_Cliente": 45000.00, "Precio_Tecnico": 40000.00, "Cantidad_Stock": 0},
   {"Codigo": "gas010", "Producto": "GAS R22 X 1KG NECTON", "Precio_Cliente": 42570.00, "Precio_Tecnico": 38610.00, "Cantidad_Stock": 105},
   {"Codigo": "r290", "Producto": "GAS R290 LATA 420G", "Precio_Cliente": 15444.00, "Precio_Tecnico": 12375.00, "Cantidad_Stock": 2},
   {"Codigo": "gasr32", "Producto": "GAS R32  680G", "Precio_Cliente": 29600.00, "Precio_Tecnico": 26600.00, "Cantidad_Stock": 4},
