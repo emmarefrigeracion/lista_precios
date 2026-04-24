f25c12c08f772fc8c311bb47a1d5467e4989c575
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-24 11:41:59 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -235,7 +235,7 @@
   {"Codigo": "mang011", "Producto": "JGO. ,AMGUERA X 3 R-410", "Precio_Cliente": 39600.00, "Precio_Tecnico": 34254.00, "Cantidad_Stock": 0},
   {"Codigo": "mang010", "Producto": "JGO. DE MANGUERA X3 R-22", "Precio_Cliente": 16137.00, "Precio_Tecnico": 15147.00, "Cantidad_Stock": 0},
   {"Codigo": "1-35-14655", "Producto": "JUEGO CARBONES NACIONALES  BUENO  (BOLSA PLATEADA)", "Precio_Cliente": 9504.00, "Precio_Tecnico": 8217.00, "Cantidad_Stock": 0},
-  {"Codigo": "man5", "Producto": "JUEGO DE MANGUERA X3 R410", "Precio_Cliente": 32670.00, "Precio_Tecnico": 23265.00, "Cantidad_Stock": 1},
+  {"Codigo": "man5", "Producto": "JUEGO DE MANGUERA X3 R410", "Precio_Cliente": 32670.00, "Precio_Tecnico": 23265.00, "Cantidad_Stock": 0},
   {"Codigo": "6-13-1127", "Producto": "JUEGO MANG. CON  LLAMA Y SOPLETE AUTOGENA", "Precio_Cliente": 63712.44, "Precio_Tecnico": 53032.32, "Cantidad_Stock": 1},
   {"Codigo": "kit010", "Producto": "KIT FIJACION DEL 8", "Precio_Cliente": 5666.76, "Precio_Tecnico": 4811.40, "Cantidad_Stock": 99},
   {"Codigo": "kit002", "Producto": "KIT INSTALACION 1/2-1/4 CON AISLANTE NEGRO Y CINTA TACSA", "Precio_Cliente": 76950.00, "Precio_Tecnico": 71820.00, "Cantidad_Stock": 89},
