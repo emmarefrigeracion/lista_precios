125194adcf825a52a278c88c3f2c5595b5f97ed6
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-29 09:15:50 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -351,7 +351,7 @@
   {"Codigo": "ret005", "Producto": "RETEN  CONSUL   10625 (PARA RODAMIENTO 6006)", "Precio_Cliente": 5940.00, "Precio_Tecnico": 4455.00, "Cantidad_Stock": 4},
   {"Codigo": "1-34-052372", "Producto": "RETEN 21X40X10", "Precio_Cliente": 3200.00, "Precio_Tecnico": 2500.00, "Cantidad_Stock": 22},
   {"Codigo": "ret015", "Producto": "RETEN 25*50*10 VUPRE ESPAÑA", "Precio_Cliente": 0.00, "Precio_Tecnico": 0.00, "Cantidad_Stock": 6},
-  {"Codigo": "1-34-0523106", "Producto": "RETEN 25X47X10", "Precio_Cliente": 2574.00, "Precio_Tecnico": 1683.00, "Cantidad_Stock": 0},
+  {"Codigo": "1-34-0523106", "Producto": "RETEN 25X47X10", "Precio_Cliente": 2574.00, "Precio_Tecnico": 1683.00, "Cantidad_Stock": 10},
   {"Codigo": "retensav", "Producto": "RETEN 25X47X10 SAV", "Precio_Cliente": 3000.00, "Precio_Tecnico": 2300.00, "Cantidad_Stock": 17},
   {"Codigo": "1-34-1026227", "Producto": "RETEN 30X52X10  SAV", "Precio_Cliente": 3000.00, "Precio_Tecnico": 2400.00, "Cantidad_Stock": 38},
   {"Codigo": "ret001", "Producto": "RETEN 30X64X17 CONSUL", "Precio_Cliente": 6100.00, "Precio_Tecnico": 4500.00, "Cantidad_Stock": 0},
