3c88a8ab033f88c01ea5c3d7026a37d509e4e320
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-04-29 09:05:54 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -417,7 +417,7 @@
   {"Codigo": "EJE004", "Producto": "SOPORTE BOSH 03/03", "Precio_Cliente": 44055.00, "Precio_Tecnico": 34155.00, "Cantidad_Stock": 2},
   {"Codigo": "lg001", "Producto": "SOPORTE LG  8.5KG  (6305/06)", "Precio_Cliente": 74250.00, "Precio_Tecnico": 67815.00, "Cantidad_Stock": 1},
   {"Codigo": "eje015", "Producto": "SOPORTE LONGVIE", "Precio_Cliente": 143748.00, "Precio_Tecnico": 136125.00, "Cantidad_Stock": 1},
-  {"Codigo": "eje021", "Producto": "SOPORTE PATRICK LPK 6/7 KG (CONICO O RECTO)", "Precio_Cliente": 48411.00, "Precio_Tecnico": 42372.00, "Cantidad_Stock": 5},
+  {"Codigo": "eje021", "Producto": "SOPORTE PATRICK LPK 6/7 KG (CONICO O RECTO)", "Precio_Cliente": 46400.00, "Precio_Tecnico": 41000.00, "Cantidad_Stock": 4},
   {"Codigo": "eje22", "Producto": "SOPORTE PATRICK LPK 8 KG", "Precio_Cliente": 29403.00, "Precio_Tecnico": 43857.00, "Cantidad_Stock": 2},
   {"Codigo": "sam008", "Producto": "SOPORTE SAMSUNG CARA PLANA  ROD 05/06", "Precio_Cliente": 51000.00, "Precio_Tecnico": 38600.00, "Cantidad_Stock": 1},
   {"Codigo": "saminv", "Producto": "SOPORTE SAMSUNG INVERTER", "Precio_Cliente": 49005.00, "Precio_Tecnico": 42800.00, "Cantidad_Stock": 1},
