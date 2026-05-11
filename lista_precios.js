f89cc34db33b0d13971532316b46d54daf125d53
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-05-11 11:26:45 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -515,7 +515,7 @@
   {"Codigo": "v8", "Producto": "VALVULA INVERSORA 3/8 X 1/2", "Precio_Cliente": 42100.00, "Precio_Tecnico": 37300.00, "Cantidad_Stock": 2},
   {"Codigo": "val001", "Producto": "VALVULA PARA LATA  C/OVULO", "Precio_Cliente": 7425.00, "Precio_Tecnico": 6435.00, "Cantidad_Stock": 0},
   {"Codigo": "val002", "Producto": "VALVULA PARA LATA C/VOLANTE", "Precio_Cliente": 18000.00, "Precio_Tecnico": 16600.00, "Cantidad_Stock": 0},
-  {"Codigo": "4-12-125", "Producto": "VARILLA  DE PLATA", "Precio_Cliente": 2500.00, "Precio_Tecnico": 2000.00, "Cantidad_Stock": 14},
+  {"Codigo": "4-12-125", "Producto": "VARILLA  DE PLATA", "Precio_Cliente": 2500.00, "Precio_Tecnico": 2200.00, "Cantidad_Stock": 34},
   {"Codigo": "VAR002", "Producto": "VARILLA DE ALUMINIO CON FUMDANTE", "Precio_Cliente": 9207.00, "Precio_Tecnico": 7029.00, "Cantidad_Stock": 0},
   {"Codigo": "mec001", "Producto": "VASTAGO 45MM (PARA  MECHA  DE  COPA)", "Precio_Cliente": 29304.00, "Precio_Tecnico": 24552.00, "Cantidad_Stock": 1},
   {"Codigo": "visor001", "Producto": "VISOR DREAN NEXT 8 KG (DISPLAY)", "Precio_Cliente": 42372.00, "Precio_Tecnico": 35244.00, "Cantidad_Stock": 0}
