e9c153aa448641828135ee89b15be2ae0b243067
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-05-22 16:50:37 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -368,7 +368,7 @@
   {"Codigo": "1-34-1026227", "Producto": "RETEN 30X52X10  SAV", "Precio_Cliente": 3000.00, "Precio_Tecnico": 2400.00, "Cantidad_Stock": 36},
   {"Codigo": "ret001", "Producto": "RETEN 30X64X17 CONSUL", "Precio_Cliente": 6100.00, "Precio_Tecnico": 4500.00, "Cantidad_Stock": 0},
   {"Codigo": "retconc", "Producto": "RETEN CONCEPT ORIG 25/55/10", "Precio_Cliente": 8900.00, "Precio_Tecnico": 7500.00, "Cantidad_Stock": 2},
-  {"Codigo": "1-38-11286", "Producto": "RETEN LG 35X66X10 LAVARROPA", "Precio_Cliente": 6435.00, "Precio_Tecnico": 4752.00, "Cantidad_Stock": 0},
+  {"Codigo": "1-38-11286", "Producto": "RETEN LG 35X66X10 LAVARROPA", "Precio_Cliente": 6435.00, "Precio_Tecnico": 4752.00, "Cantidad_Stock": 10},
   {"Codigo": "lg2", "Producto": "RETEN LG 37*76*10", "Precio_Cliente": 5900.00, "Precio_Tecnico": 3900.00, "Cantidad_Stock": 5},
   {"Codigo": "patr6", "Producto": "RETEN PATRICK  8KG  47/88/10", "Precio_Cliente": 7200.00, "Precio_Tecnico": 6300.00, "Cantidad_Stock": 5},
   {"Codigo": "r37", "Producto": "RETEN PATRICK 37*66*10 6KG", "Precio_Cliente": 4300.00, "Precio_Tecnico": 3300.00, "Cantidad_Stock": 14},
