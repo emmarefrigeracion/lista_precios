18899baa42a69412b115af5b007e5f9125539ae8
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-05-22 10:27:12 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -110,7 +110,7 @@
   {"Codigo": "4-22-122008", "Producto": "CAPACITOR DUAL 50+6", "Precio_Cliente": 17900.00, "Precio_Tecnico": 14700.00, "Cantidad_Stock": 4},
   {"Codigo": "cap015", "Producto": "CAPILAR 1.2   (3 METRO)", "Precio_Cliente": 10600.00, "Precio_Tecnico": 8200.00, "Cantidad_Stock": 3},
   {"Codigo": "capi08", "Producto": "CAPILAR COBRE 0.8   (3 METROS)", "Precio_Cliente": 7600.00, "Precio_Tecnico": 6200.00, "Cantidad_Stock": 1},
-  {"Codigo": "car001", "Producto": "CARBONES 12.5-13.5 EN CAJA ALFA CARBON", "Precio_Cliente": 16000.00, "Precio_Tecnico": 14000.00, "Cantidad_Stock": 11},
+  {"Codigo": "car001", "Producto": "CARBONES 12.5-13.5 EN CAJA ALFA CARBON", "Precio_Cliente": 16000.00, "Precio_Tecnico": 13800.00, "Cantidad_Stock": 11},
   {"Codigo": "car1", "Producto": "CARBONES LE CARD", "Precio_Cliente": 11100.00, "Precio_Tecnico": 9900.00, "Cantidad_Stock": 1},
   {"Codigo": "peltier2", "Producto": "CELDA PELTIER 40*40MM TEC1-12708", "Precio_Cliente": 23000.00, "Precio_Tecnico": 19500.00, "Cantidad_Stock": 2},
   {"Codigo": "cin15", "Producto": "CINTA AILSADORA NEGRA 20 METROS", "Precio_Cliente": 6500.00, "Precio_Tecnico": 5200.00, "Cantidad_Stock": 9},
