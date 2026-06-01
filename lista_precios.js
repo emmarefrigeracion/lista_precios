9513bdcf5cd34f3c33e682f8c6945f7981441906
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-06-01 10:51:02 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -178,7 +178,7 @@
   {"Codigo": "ele2", "Producto": "ELECTROVALVULA 2 VIAS 90º", "Precio_Cliente": 16500.00, "Precio_Tecnico": 13500.00, "Cantidad_Stock": 4},
   {"Codigo": "ele005", "Producto": "ELECTROVALVULA FICHA DREAN BLUE ORIGINAL", "Precio_Cliente": 23000.00, "Precio_Tecnico": 19900.00, "Cantidad_Stock": 7},
   {"Codigo": "eli020", "Producto": "ELICE 100MM (FORZADOR GAFA)", "Precio_Cliente": 7425.00, "Precio_Tecnico": 4950.00, "Cantidad_Stock": 2},
-  {"Codigo": "em5", "Producto": "EMBRAGUE DREAN CONCEPT COMPLETO", "Precio_Cliente": 4900.13, "Precio_Tecnico": 3915.00, "Cantidad_Stock": 5},
+  {"Codigo": "em5", "Producto": "EMBRAGUE DREAN CONCEPT COMPLETO", "Precio_Cliente": 7500.00, "Precio_Tecnico": 6900.00, "Cantidad_Stock": 5},
   {"Codigo": "tor014", "Producto": "EMBRAGUE GAFA 6-7 KG", "Precio_Cliente": 18900.00, "Precio_Tecnico": 16400.00, "Cantidad_Stock": 1},
   {"Codigo": "esc01", "Producto": "ESCAREADOR PARA CAÑOS", "Precio_Cliente": 70000.00, "Precio_Tecnico": 6000.00, "Cantidad_Stock": 3},
   {"Codigo": "est001", "Producto": "ESTUFA ELECTRICA HORIZONTAL-VERTICAL", "Precio_Cliente": 21820.59, "Precio_Tecnico": 0.00, "Cantidad_Stock": 8},
