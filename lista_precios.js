6022eff594b86e577038488ba7fe3cc18c7a022a
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-06-10 10:15:39 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -249,7 +249,7 @@
   {"Codigo": "man5", "Producto": "JUEGO DE MANGUERA X3 R410", "Precio_Cliente": 32670.00, "Precio_Tecnico": 23265.00, "Cantidad_Stock": 0},
   {"Codigo": "6-13-1127", "Producto": "JUEGO MANG. CON  LLAMA Y SOPLETE AUTOGENA", "Precio_Cliente": 63712.44, "Precio_Tecnico": 53032.32, "Cantidad_Stock": 1},
   {"Codigo": "kit010", "Producto": "KIT FIJACION DEL 8", "Precio_Cliente": 5666.76, "Precio_Tecnico": 4811.40, "Cantidad_Stock": 95},
-  {"Codigo": "kit002", "Producto": "KIT INSTALACION 1/2-1/4 CON AISLANTE NEGRO Y CINTA TACSA", "Precio_Cliente": 76950.00, "Precio_Tecnico": 71820.00, "Cantidad_Stock": 88},
+  {"Codigo": "kit002", "Producto": "KIT INSTALACION 1/2-1/4 CON AISLANTE NEGRO Y CINTA TACSA", "Precio_Cliente": 85000.00, "Precio_Tecnico": 79000.00, "Cantidad_Stock": 88},
   {"Codigo": "kit004", "Producto": "KIT INSTALACION 1/4-3/8 CON AISLANTE NEGRO Y CINTA TACSA", "Precio_Cliente": 75000.00, "Precio_Tecnico": 70000.00, "Cantidad_Stock": 93},
   {"Codigo": "kit012", "Producto": "KIT INSTALACION 3/8-5/8 CON AISLANTE NEGRO Y CINTA TACSA", "Precio_Cliente": 0.00, "Precio_Tecnico": 0.00, "Cantidad_Stock": 2},
   {"Codigo": "lam01", "Producto": "LAMPARA ALOGENA", "Precio_Cliente": 3500.00, "Precio_Tecnico": 2900.00, "Cantidad_Stock": 1},
