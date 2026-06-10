560913d07aff4f604fc1ae35bea8d5b29d407510
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-06-10 10:21:09 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -251,6 +251,7 @@
   {"Codigo": "kit010", "Producto": "KIT FIJACION DEL 8", "Precio_Cliente": 5666.76, "Precio_Tecnico": 4811.40, "Cantidad_Stock": 95},
   {"Codigo": "kit002", "Producto": "KIT INSTALACION 1/2-1/4 CON AISLANTE NEGRO Y CINTA TACSA", "Precio_Cliente": 85000.00, "Precio_Tecnico": 79000.00, "Cantidad_Stock": 88},
   {"Codigo": "kit004", "Producto": "KIT INSTALACION 1/4-3/8 CON AISLANTE NEGRO Y CINTA TACSA", "Precio_Cliente": 75000.00, "Precio_Tecnico": 70000.00, "Cantidad_Stock": 93},
+  {"Codigo": "kit1", "Producto": "KIT INSTALACION 1/4-5/8", "Precio_Cliente": 98000.00, "Precio_Tecnico": 92000.00, "Cantidad_Stock": 0},
   {"Codigo": "kit012", "Producto": "KIT INSTALACION 3/8-5/8 CON AISLANTE NEGRO Y CINTA TACSA", "Precio_Cliente": 110000.00, "Precio_Tecnico": 102000.00, "Cantidad_Stock": 2},
   {"Codigo": "lam01", "Producto": "LAMPARA ALOGENA", "Precio_Cliente": 3500.00, "Precio_Tecnico": 2900.00, "Cantidad_Stock": 1},
   {"Codigo": "lam02", "Producto": "LAMPARA LED", "Precio_Cliente": 4600.00, "Precio_Tecnico": 3900.00, "Cantidad_Stock": 2},
