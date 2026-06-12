37f5f72c05754d66efe22fd87a76c6b674c2a364
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-06-12 17:57:42 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -79,6 +79,7 @@
   {"Codigo": "cable", "Producto": "CABLE TPR 3*1.5", "Precio_Cliente": 3100.00, "Precio_Tecnico": 2400.00, "Cantidad_Stock": 10},
   {"Codigo": "8-3-BI-5.15", "Producto": "CABLE TPR 5 X 1.5MM  POR METRO", "Precio_Cliente": 4500.00, "Precio_Tecnico": 3200.00, "Cantidad_Stock": 100},
   {"Codigo": "8-3-TPR525C", "Producto": "CABLE TPR 5 X 2,5 MM POR METRO", "Precio_Cliente": 5900.00, "Precio_Tecnico": 4900.00, "Cantidad_Stock": 20},
+  {"Codigo": "capa05", "Producto": "CACITOR 5 MF", "Precio_Cliente": 3900.00, "Precio_Tecnico": 2900.00, "Cantidad_Stock": 8},
   {"Codigo": "1-18-0105", "Producto": "CAJA CHAPON ALTERNATIVA SANYO/GAFA/DAEWOO", "Precio_Cliente": 94347.00, "Precio_Tecnico": 75537.00, "Cantidad_Stock": 0},
   {"Codigo": "caja1", "Producto": "CAJA DE ENGRANAJE GAFA/ELECTROLUX", "Precio_Cliente": 72500.00, "Precio_Tecnico": 67500.00, "Cantidad_Stock": 1},
   {"Codigo": "caj011", "Producto": "CAJA GAFA CHAPON ORIGINAL", "Precio_Cliente": 118800.00, "Precio_Tecnico": 100485.00, "Cantidad_Stock": 0},
