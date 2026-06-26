2cb0e79f3cc1f2525c31ac3b500a2b5181b4c650
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-06-26 17:35:27 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -302,6 +302,7 @@
   {"Codigo": "014", "Producto": "MOTOR DOMPER 1/3 HP FAMILIAR", "Precio_Cliente": 115830.00, "Precio_Tecnico": 92763.00, "Cantidad_Stock": 3},
   {"Codigo": "don1", "Producto": "MOTOR DONPER 1/3+HP", "Precio_Cliente": 0.00, "Precio_Tecnico": 0.00, "Cantidad_Stock": 0},
   {"Codigo": "3989", "Producto": "MOTOR DONPER 1/3HP", "Precio_Cliente": 0.00, "Precio_Tecnico": 0.00, "Cantidad_Stock": 1},
+  {"Codigo": "3988", "Producto": "MOTOR DONPER 1/4+", "Precio_Cliente": 0.00, "Precio_Tecnico": 0.00, "Cantidad_Stock": 2},
   {"Codigo": "mot1", "Producto": "MOTOR DREAN 7 CONTACTOS", "Precio_Cliente": 175000.00, "Precio_Tecnico": 163000.00, "Cantidad_Stock": 2},
   {"Codigo": "motdre", "Producto": "MOTOR DREAN ORIGINAL 6 CONTACTOS", "Precio_Cliente": 167900.00, "Precio_Tecnico": 160380.00, "Cantidad_Stock": 4},
   {"Codigo": "mot022", "Producto": "MOTOR EMBRACO 1/3HP R134", "Precio_Cliente": 0.00, "Precio_Tecnico": 0.00, "Cantidad_Stock": 0},
