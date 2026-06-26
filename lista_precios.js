b99b88ee608dcb279863d7a00983ec5fb73425ca
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-06-26 12:10:10 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -524,7 +524,7 @@
   {"Codigo": "tim10", "Producto": "TIMER HORNO ELECTRICO", "Precio_Cliente": 11500.00, "Precio_Tecnico": 9500.00, "Cantidad_Stock": 0},
   {"Codigo": "tim2", "Producto": "TIMER LAVARROPA CODINI", "Precio_Cliente": 29000.00, "Precio_Tecnico": 23000.00, "Cantidad_Stock": 2},
   {"Codigo": "tim020", "Producto": "TIMER NO FROST ORIGINAL", "Precio_Cliente": 23900.00, "Precio_Tecnico": 19900.00, "Cantidad_Stock": 1},
-  {"Codigo": "tim19", "Producto": "TIMER PROGRAMABLE  NO FROST UNIVERSAL", "Precio_Cliente": 19701.00, "Precio_Tecnico": 15345.00, "Cantidad_Stock": 1},
+  {"Codigo": "tim19", "Producto": "TIMER PROGRAMABLE  NO FROST UNIVERSAL (NEGRO)", "Precio_Cliente": 19701.00, "Precio_Tecnico": 15345.00, "Cantidad_Stock": 1},
   {"Codigo": "tir001", "Producto": "TIRADOR AURORA  ELECTROLUX EW", "Precio_Cliente": 8900.00, "Precio_Tecnico": 6200.00, "Cantidad_Stock": 2},
   {"Codigo": "tir002", "Producto": "TIRADOR AURORA/DREAN", "Precio_Cliente": 5900.00, "Precio_Tecnico": 4900.00, "Cantidad_Stock": 4},
   {"Codigo": "tir004", "Producto": "TIRADOR DREAN 166 (SIN MANIJA)", "Precio_Cliente": 4500.00, "Precio_Tecnico": 3700.00, "Cantidad_Stock": 1},
