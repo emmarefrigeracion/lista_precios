5203220cb247d1c590aa5ca677ea5fe2b0926353
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-06-29 09:45:08 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -337,7 +337,7 @@
   {"Codigo": "p2", "Producto": "PERILLA AZUL SECARROPAS KOHINOOR", "Precio_Cliente": 3000.00, "Precio_Tecnico": 2200.00, "Cantidad_Stock": 5},
   {"Codigo": "per2", "Producto": "PERILLA BLUE/GOLD", "Precio_Cliente": 5600.00, "Precio_Tecnico": 3600.00, "Cantidad_Stock": 2},
   {"Codigo": "p3", "Producto": "PERILLA CODINI ORIGINAL", "Precio_Cliente": 3200.00, "Precio_Tecnico": 2800.00, "Cantidad_Stock": 6},
-  {"Codigo": "00826", "Producto": "PERILLA DREAN FAMILY", "Precio_Cliente": 9500.00, "Precio_Tecnico": 7800.00, "Cantidad_Stock": 4},
+  {"Codigo": "3158", "Producto": "PERILLA DREAN FAMILY", "Precio_Cliente": 9500.00, "Precio_Tecnico": 7800.00, "Cantidad_Stock": 4},
   {"Codigo": "ter19", "Producto": "PERILLA PARA TERMOSTATO", "Precio_Cliente": 2900.00, "Precio_Tecnico": 1900.00, "Cantidad_Stock": 1},
   {"Codigo": "per04", "Producto": "PERILLA PATRIOT", "Precio_Cliente": 15000.00, "Precio_Tecnico": 1000.00, "Cantidad_Stock": 4},
   {"Codigo": "p1", "Producto": "PERILLA QV SECARROPA ALLADIO", "Precio_Cliente": 3200.00, "Precio_Tecnico": 2800.00, "Cantidad_Stock": 5},
