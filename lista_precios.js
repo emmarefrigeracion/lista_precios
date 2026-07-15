7995b0b90f9c0c5ec1f54a68fe63de2e6a8b6f89
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-07-15 13:11:46 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -59,7 +59,7 @@
   {"Codigo": "blo008", "Producto": "BLOCAPUERTA TIPO ELECTROLUX", "Precio_Cliente": 19900.00, "Precio_Tecnico": 16700.00, "Cantidad_Stock": 2},
   {"Codigo": "bom01", "Producto": "BOMBA ASKOLL", "Precio_Cliente": 13500.00, "Precio_Tecnico": 11500.00, "Cantidad_Stock": 59},
   {"Codigo": "bom2", "Producto": "BOMBA DESAGOTE 1/2 GIRO", "Precio_Cliente": 19000.00, "Precio_Tecnico": 17000.00, "Cantidad_Stock": 2},
-  {"Codigo": "bom3", "Producto": "BOMBA DESAGOTE 3 TRABAS", "Precio_Cliente": 23100.00, "Precio_Tecnico": 20700.00, "Cantidad_Stock": 2},
+  {"Codigo": "bom3", "Producto": "BOMBA DESAGOTE 3 TRABAS", "Precio_Cliente": 21800.00, "Precio_Tecnico": 18600.00, "Cantidad_Stock": 3},
   {"Codigo": "bom4", "Producto": "BOMBA DESAGOTE 4 TRABAS", "Precio_Cliente": 37700.00, "Precio_Tecnico": 35000.00, "Cantidad_Stock": 3},
   {"Codigo": "b5", "Producto": "BOTONERA  DREAN NEXT/AURORA", "Precio_Cliente": 7500.00, "Precio_Tecnico": 5200.00, "Cantidad_Stock": 14},
   {"Codigo": "b9", "Producto": "BOTONERA WHIRLPOOL 7 BOT", "Precio_Cliente": 8500.00, "Precio_Tecnico": 7600.00, "Cantidad_Stock": 0},
