4e379aba51343a42614aec9195a2143e77926a54
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-07-28 11:10:55 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -555,7 +555,7 @@
   {"Codigo": "ter021", "Producto": "TERMOSTATO EXHIBIDORA BLUESTAR", "Precio_Cliente": 16500.00, "Precio_Tecnico": 13500.00, "Cantidad_Stock": 2},
   {"Codigo": "terfree", "Producto": "TERMOSTATO FREEZER 53670-4", "Precio_Cliente": 19107.00, "Precio_Tecnico": 16038.00, "Cantidad_Stock": 3},
   {"Codigo": "003", "Producto": "TERMOSTATO K59", "Precio_Cliente": 16000.00, "Precio_Tecnico": 12100.00, "Cantidad_Stock": 7},
-  {"Codigo": "ter59", "Producto": "TERMOSTATO K59 X 2 MTRS", "Precio_Cliente": 17000.00, "Precio_Tecnico": 12800.00, "Cantidad_Stock": 4},
+  {"Codigo": "ter59", "Producto": "TERMOSTATO K59 X 2 MTRS", "Precio_Cliente": 17000.00, "Precio_Tecnico": 13600.00, "Cantidad_Stock": 4},
   {"Codigo": "ter26", "Producto": "TERMOSTATO KDF26 (K59 ALTERNATIVO)", "Precio_Cliente": 19000.00, "Precio_Tecnico": 12700.00, "Cantidad_Stock": 1},
   {"Codigo": "ter30", "Producto": "TERMOSTATO KDF30 ORIGINAL DE GAFA", "Precio_Cliente": 28000.00, "Precio_Tecnico": 22800.00, "Cantidad_Stock": 2},
   {"Codigo": "ter1", "Producto": "TERMOSTATO NO FROST GAFA ( PARA HELADERA NO FROST CONVERTIDA A MECANICA)", "Precio_Cliente": 24354.00, "Precio_Tecnico": 20988.00, "Cantidad_Stock": 2},
