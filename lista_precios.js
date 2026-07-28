552c091118c95c3fa087f889b45fb14e8d54cde7
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-07-28 10:50:15 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -569,7 +569,7 @@
   {"Codigo": "dis002", "Producto": "TERMOSTATO RC 42600-2 BEBEDERO/DISPENSER", "Precio_Cliente": 17500.00, "Precio_Tecnico": 14700.00, "Cantidad_Stock": 1},
   {"Codigo": "ter5", "Producto": "TERMOSTATO ROBERTSHAW RC 53670", "Precio_Cliente": 28908.00, "Precio_Tecnico": 25740.00, "Cantidad_Stock": 1},
   {"Codigo": "tim10", "Producto": "TIMER HORNO ELECTRICO", "Precio_Cliente": 11500.00, "Precio_Tecnico": 9900.00, "Cantidad_Stock": 2},
-  {"Codigo": "tim2", "Producto": "TIMER LAVARROPA CODINI", "Precio_Cliente": 29000.00, "Precio_Tecnico": 23000.00, "Cantidad_Stock": 2},
+  {"Codigo": "tim2", "Producto": "TIMER LAVARROPA CODINI", "Precio_Cliente": 15700.00, "Precio_Tecnico": 11600.00, "Cantidad_Stock": 2},
   {"Codigo": "tim020", "Producto": "TIMER NO FROST ORIGINAL", "Precio_Cliente": 23900.00, "Precio_Tecnico": 19900.00, "Cantidad_Stock": 1},
   {"Codigo": "tim19", "Producto": "TIMER PROGRAMABLE  NO FROST UNIVERSAL (NEGRO)", "Precio_Cliente": 16999.71, "Precio_Tecnico": 14000.35, "Cantidad_Stock": 4},
   {"Codigo": "tir001", "Producto": "TIRADOR AURORA  ELECTROLUX EW", "Precio_Cliente": 8900.00, "Precio_Tecnico": 6200.00, "Cantidad_Stock": 2},
