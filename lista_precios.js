df65bb5d16bcd3477bfd7747cf594f480309a9fb
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-07-30 16:40:33 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -455,7 +455,7 @@
   {"Codigo": "ntn6202", "Producto": "RODAMIENTO NTN 6202 ALTA VEL (ZZC3)", "Precio_Cliente": 5100.00, "Precio_Tecnico": 3600.00, "Cantidad_Stock": 4},
   {"Codigo": "rod002", "Producto": "RODAMIENTO NTN 6203", "Precio_Cliente": 5200.00, "Precio_Tecnico": 4500.00, "Cantidad_Stock": 100},
   {"Codigo": "rod003", "Producto": "RODAMIENTO NTN 6204", "Precio_Cliente": 6900.00, "Precio_Tecnico": 5800.00, "Cantidad_Stock": 1000},
-  {"Codigo": "rod005", "Producto": "RODAMIENTO NTN 6205", "Precio_Cliente": 7200.00, "Precio_Tecnico": 6400.00, "Cantidad_Stock": 9},
+  {"Codigo": "rod005", "Producto": "RODAMIENTO NTN 6205", "Precio_Cliente": 7900.00, "Precio_Tecnico": 7000.00, "Cantidad_Stock": 100},
   {"Codigo": "rod006", "Producto": "RODAMIENTO NTN 6206", "Precio_Cliente": 13900.00, "Precio_Tecnico": 11000.00, "Cantidad_Stock": 100},
   {"Codigo": "1-39-6202SKF", "Producto": "RODAMIENTO SKF  6202", "Precio_Cliente": 5700.00, "Precio_Tecnico": 4800.00, "Cantidad_Stock": 0},
   {"Codigo": "1-39-6204SKF", "Producto": "RODAMIENTO SKF  6204", "Precio_Cliente": 7920.00, "Precio_Tecnico": 6138.00, "Cantidad_Stock": 0},
