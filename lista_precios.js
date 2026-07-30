59843f89d163483d2d6beefba0516f1cbbe7b336
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-07-30 16:30:56 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -572,6 +572,7 @@
   {"Codigo": "t7", "Producto": "TERMOSTATO PARA HORNO ELECTRICO 1/2 GIRO 250°", "Precio_Cliente": 7000.00, "Precio_Tecnico": 5900.00, "Cantidad_Stock": 6},
   {"Codigo": "dis002", "Producto": "TERMOSTATO RC 42600-2 BEBEDERO/DISPENSER", "Precio_Cliente": 17500.00, "Precio_Tecnico": 14700.00, "Cantidad_Stock": 1},
   {"Codigo": "ter5", "Producto": "TERMOSTATO ROBERTSHAW RC 53670", "Precio_Cliente": 28908.00, "Precio_Tecnico": 25740.00, "Cantidad_Stock": 1},
+  {"Codigo": "TER11", "Producto": "TERMOSTATO ROBERTSHAW TSV 2012", "Precio_Cliente": 28700.00, "Precio_Tecnico": 33800.00, "Cantidad_Stock": 2},
   {"Codigo": "tim10", "Producto": "TIMER HORNO ELECTRICO", "Precio_Cliente": 11500.00, "Precio_Tecnico": 9900.00, "Cantidad_Stock": 2},
   {"Codigo": "tim2", "Producto": "TIMER LAVARROPA CODINI", "Precio_Cliente": 15700.00, "Precio_Tecnico": 11600.00, "Cantidad_Stock": 2},
   {"Codigo": "tim020", "Producto": "TIMER NO FROST ORIGINAL", "Precio_Cliente": 23900.00, "Precio_Tecnico": 19900.00, "Cantidad_Stock": 1},
