c9ad15b9820ecfecb3ec8a54e7c733df8926c7b3
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-07-30 16:18:18 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -27,6 +27,7 @@
   {"Codigo": "am5", "Producto": "AMORTIGUADORES DREAN GOLD LARGOS/CANDY", "Precio_Cliente": 18900.00, "Precio_Tecnico": 16900.00, "Cantidad_Stock": 2},
   {"Codigo": "am2", "Producto": "AMORTIGUADORES PATRICK  PRECIO UNITARIO", "Precio_Cliente": 12100.00, "Precio_Tecnico": 9200.00, "Cantidad_Stock": 0},
   {"Codigo": "ani", "Producto": "ANILLO DE CHAPA TERMOACTUADOR CONCEPT", "Precio_Cliente": 5500.00, "Precio_Tecnico": 4500.00, "Cantidad_Stock": 8},
+  {"Codigo": "A02", "Producto": "ARANDELA DREAN 8KG (P CONO)", "Precio_Cliente": 1800.00, "Precio_Tecnico": 1200.00, "Cantidad_Stock": 6},
   {"Codigo": "aran2", "Producto": "ARANDELAS  GRANDES", "Precio_Cliente": 30.00, "Precio_Tecnico": 27.00, "Cantidad_Stock": 100},
   {"Codigo": "3694", "Producto": "ARANDELAS POLEA DREAN CONCEP", "Precio_Cliente": 1900.00, "Precio_Tecnico": 1500.00, "Cantidad_Stock": 3},
   {"Codigo": "fle5", "Producto": "ARCO FLEJE DE FRENO PARA SECARROPA KOHINOOR", "Precio_Cliente": 6900.00, "Precio_Tecnico": 5800.00, "Cantidad_Stock": 3},
