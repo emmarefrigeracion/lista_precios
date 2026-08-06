dee449c77e5736ce04df3b14c72d93ce4e8a3f6d
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-08-06 17:17:37 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -398,7 +398,7 @@
   {"Codigo": "pol004", "Producto": "POLEA MOTOR GAFA", "Precio_Cliente": 18000.00, "Precio_Tecnico": 14500.00, "Cantidad_Stock": 1},
   {"Codigo": "pol002", "Producto": "POLEA SEMIAUTOMATICO CON ESPINA (CHIQUITA)", "Precio_Cliente": 4257.00, "Precio_Tecnico": 2475.00, "Cantidad_Stock": 0},
   {"Codigo": "3679", "Producto": "POLEA TAMBOR DREAN 8 KG DOS CARAS", "Precio_Cliente": 18700.00, "Precio_Tecnico": 16200.00, "Cantidad_Stock": 2},
-  {"Codigo": "3678", "Producto": "POLEA TAMBOR DREAN BLUE 8KG CONICO", "Precio_Cliente": 18700.00, "Precio_Tecnico": 16200.00, "Cantidad_Stock": 1},
+  {"Codigo": "3678", "Producto": "POLEA TAMBOR DREAN BLUE 8KG CONICO", "Precio_Cliente": 19100.00, "Precio_Tecnico": 17200.00, "Cantidad_Stock": 4},
   {"Codigo": "pol006", "Producto": "POLEAN DREAN CONCEPT 7 VIAS", "Precio_Cliente": 34650.00, "Precio_Tecnico": 24750.00, "Cantidad_Stock": 1},
   {"Codigo": "poli300", "Producto": "POLIURETANO EXPANDIBLE 300ML", "Precio_Cliente": 9300.00, "Precio_Tecnico": 8400.00, "Cantidad_Stock": 5},
   {"Codigo": "porta", "Producto": "PORTA RODAMIENTO  DREAN GOLD ORIGINAL", "Precio_Cliente": 23500.00, "Precio_Tecnico": 20900.00, "Cantidad_Stock": 4},
