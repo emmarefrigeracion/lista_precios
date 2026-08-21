35891f964dccafc0d09e01962cfb8d6fbf6f81dc
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-08-21 11:40:11 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -477,7 +477,7 @@
   {"Codigo": "ntn6201", "Producto": "RODAMIENTOS NTN 6201 ALTA VEL  (ZZC3)", "Precio_Cliente": 4500.00, "Precio_Tecnico": 3900.00, "Cantidad_Stock": 6},
   {"Codigo": "rod001", "Producto": "RODMAMIENTO NTN 6202", "Precio_Cliente": 3500.00, "Precio_Tecnico": 2900.00, "Cantidad_Stock": 26},
   {"Codigo": "38", "Producto": "ROLLO DE CAÑO 3/8 (15M)", "Precio_Cliente": 0.00, "Precio_Tecnico": 130000.00, "Cantidad_Stock": 100},
-  {"Codigo": "12", "Producto": "ROLLO DE CAÑO DE 1/2 (15M)", "Precio_Cliente": 0.00, "Precio_Tecnico": 171800.00, "Cantidad_Stock": 100},
+  {"Codigo": "12", "Producto": "ROLLO DE CAÑO DE 1/2 (15M)", "Precio_Cliente": 199000.00, "Precio_Tecnico": 175000.00, "Cantidad_Stock": 100},
   {"Codigo": "14", "Producto": "ROLLO DE CAÑO DE 1/4 (15M)", "Precio_Cliente": 93380.00, "Precio_Tecnico": 82000.00, "Cantidad_Stock": 101},
   {"Codigo": "r6", "Producto": "ROLLO MANGUERA CRISTAL POR 25M", "Precio_Cliente": 0.00, "Precio_Tecnico": 37500.00, "Cantidad_Stock": 1000},
   {"Codigo": "1-39-6206SKF", "Producto": "RULEMAN  SKF  6206", "Precio_Cliente": 16137.00, "Precio_Tecnico": 12375.00, "Cantidad_Stock": 1},
