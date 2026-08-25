f90182508d6b04fedb6ad206b75b81186e4568bf
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-08-25 11:48:19 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -505,7 +505,7 @@
   {"Codigo": "concept", "Producto": "SOPORTE AC&C CONCEPT 5.05", "Precio_Cliente": 49000.00, "Precio_Tecnico": 45000.00, "Cantidad_Stock": 2},
   {"Codigo": "sop015", "Producto": "SOPORTE AC&C DREAN 8/10KG  (REDONDITO Y CARA PLANA)", "Precio_Cliente": 33500.00, "Precio_Tecnico": 29700.00, "Cantidad_Stock": 6},
   {"Codigo": "EJE003", "Producto": "SOPORTE AURORA 03/03 CONICO", "Precio_Cliente": 28413.00, "Precio_Tecnico": 26235.00, "Cantidad_Stock": 0},
-  {"Codigo": "sop011", "Producto": "SOPORTE BLUE 6 Y 7KG (CORONITA)", "Precio_Cliente": 28442.70, "Precio_Tecnico": 24700.00, "Cantidad_Stock": 0},
+  {"Codigo": "sop011", "Producto": "SOPORTE BLUE 6 Y 7KG (CORONITA)", "Precio_Cliente": 32000.00, "Precio_Tecnico": 29000.00, "Cantidad_Stock": 3},
   {"Codigo": "eje025", "Producto": "SOPORTE BLUE 6-7 KG ORIGINAL", "Precio_Cliente": 35000.00, "Precio_Tecnico": 28000.00, "Cantidad_Stock": 1},
   {"Codigo": "EJE026", "Producto": "SOPORTE BLUE 8.12 (8KG) ORIGINAL REDONDITO", "Precio_Cliente": 41580.00, "Precio_Tecnico": 36000.00, "Cantidad_Stock": 6},
   {"Codigo": "EJE004", "Producto": "SOPORTE BOSH 03/03", "Precio_Cliente": 36000.00, "Precio_Tecnico": 32500.00, "Cantidad_Stock": 0},
