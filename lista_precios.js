e114a9b0603834c6f7d89792b6288a38efa8ab17
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-08-27 11:17:58 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -109,7 +109,7 @@
   {"Codigo": "4-3-4558", "Producto": "CAÑO COBRE  1/2  POR METRO", "Precio_Cliente": 14200.00, "Precio_Tecnico": 13200.00, "Cantidad_Stock": 79},
   {"Codigo": "4-3-4514", "Producto": "CAÑO COBRE  1/4 POR METRO", "Precio_Cliente": 6600.00, "Precio_Tecnico": 6100.00, "Cantidad_Stock": 123},
   {"Codigo": "4-3-4538", "Producto": "CAÑO COBRE 3/8 POR METRO", "Precio_Cliente": 10500.00, "Precio_Tecnico": 9900.00, "Cantidad_Stock": 120},
-  {"Codigo": "4-3-ROLLO5/8", "Producto": "CAÑO COBRE 5/8  POR METRO", "Precio_Cliente": 16748.00, "Precio_Tecnico": 15582.00, "Cantidad_Stock": 1012},
+  {"Codigo": "4-3-ROLLO5/8", "Producto": "CAÑO COBRE 5/8  POR METRO", "Precio_Cliente": 16500.00, "Precio_Tecnico": 15500.00, "Cantidad_Stock": 1012},
   {"Codigo": "capa16", "Producto": "CAPACITOR  16MF", "Precio_Cliente": 5900.00, "Precio_Tecnico": 4000.00, "Cantidad_Stock": 8},
   {"Codigo": "capa20", "Producto": "CAPACITOR  20MF 440V", "Precio_Cliente": 6200.00, "Precio_Tecnico": 4700.00, "Cantidad_Stock": 3},
   {"Codigo": "capa25", "Producto": "CAPACITOR  25 MF", "Precio_Cliente": 6900.00, "Precio_Tecnico": 5600.00, "Cantidad_Stock": 5},
