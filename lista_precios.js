86ef232b8c83745820de08bd7ed0a3215a883bb6
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-08-27 11:16:41 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -107,7 +107,7 @@
   {"Codigo": "cal001", "Producto": "CALOVENTOR SERENA (BLANCO)", "Precio_Cliente": 25000.00, "Precio_Tecnico": 20000.00, "Cantidad_Stock": 5},
   {"Codigo": "cal002", "Producto": "CALOVENTOR SERENA (NEGRO)", "Precio_Cliente": 17000.00, "Precio_Tecnico": 0.00, "Cantidad_Stock": 3},
   {"Codigo": "4-3-4558", "Producto": "CAÑO COBRE  1/2  POR METRO", "Precio_Cliente": 14840.00, "Precio_Tecnico": 13356.00, "Cantidad_Stock": 79},
-  {"Codigo": "4-3-4514", "Producto": "CAÑO COBRE  1/4 POR METRO", "Precio_Cliente": 6300.00, "Precio_Tecnico": 5800.00, "Cantidad_Stock": 123},
+  {"Codigo": "4-3-4514", "Producto": "CAÑO COBRE  1/4 POR METRO", "Precio_Cliente": 6600.00, "Precio_Tecnico": 6100.00, "Cantidad_Stock": 123},
   {"Codigo": "4-3-4538", "Producto": "CAÑO COBRE 3/8 POR METRO", "Precio_Cliente": 11554.00, "Precio_Tecnico": 10388.00, "Cantidad_Stock": 120},
   {"Codigo": "4-3-ROLLO5/8", "Producto": "CAÑO COBRE 5/8  POR METRO", "Precio_Cliente": 16748.00, "Precio_Tecnico": 15582.00, "Cantidad_Stock": 1012},
   {"Codigo": "capa16", "Producto": "CAPACITOR  16MF", "Precio_Cliente": 5900.00, "Precio_Tecnico": 4000.00, "Cantidad_Stock": 8},
