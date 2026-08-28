ae0be7f609c79ab3300f7b7dcf13e83d2384e50d
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-08-28 10:09:01 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -504,7 +504,7 @@
   {"Codigo": "eje032", "Producto": "SOPORTE 166 ORIGINAL  REDONDITO", "Precio_Cliente": 29502.00, "Precio_Tecnico": 23760.00, "Cantidad_Stock": 2},
   {"Codigo": "EJE002", "Producto": "SOPORTE AC&C 166 DREAN  CILINDRICO", "Precio_Cliente": 27000.00, "Precio_Tecnico": 24000.00, "Cantidad_Stock": 90},
   {"Codigo": "eje011", "Producto": "SOPORTE AC&C BLUE 6-7 KG NACIONAL  (03-04)", "Precio_Cliente": 31000.00, "Precio_Tecnico": 27000.00, "Cantidad_Stock": 9},
-  {"Codigo": "concept", "Producto": "SOPORTE AC&C CONCEPT 5.05", "Precio_Cliente": 49000.00, "Precio_Tecnico": 45000.00, "Cantidad_Stock": 2},
+  {"Codigo": "concept", "Producto": "SOPORTE AC&C CONCEPT 5.05", "Precio_Cliente": 51000.00, "Precio_Tecnico": 48000.00, "Cantidad_Stock": 2},
   {"Codigo": "sop015", "Producto": "SOPORTE AC&C DREAN 8/10KG  (REDONDITO Y CARA PLANA)", "Precio_Cliente": 33500.00, "Precio_Tecnico": 29700.00, "Cantidad_Stock": 6},
   {"Codigo": "EJE003", "Producto": "SOPORTE AURORA 03/03 CONICO", "Precio_Cliente": 28413.00, "Precio_Tecnico": 26235.00, "Cantidad_Stock": 0},
   {"Codigo": "sop011", "Producto": "SOPORTE BLUE 6 Y 7KG (CORONITA)", "Precio_Cliente": 32000.00, "Precio_Tecnico": 29000.00, "Cantidad_Stock": 3},
