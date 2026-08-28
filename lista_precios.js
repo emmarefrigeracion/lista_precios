40669ef7d09cc16fd32239e8a309bf4a69596a25
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-08-28 17:49:44 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -429,7 +429,7 @@
   {"Codigo": "rel5", "Producto": "RELAY PROTECTOR EMBRACO FF12BK 1/3 HP", "Precio_Cliente": 25650.00, "Precio_Tecnico": 21330.00, "Cantidad_Stock": 1},
   {"Codigo": "rel6", "Producto": "RELAY PROTECTOR EMBRACO FF8.5BK 1/4HP", "Precio_Cliente": 25650.00, "Precio_Tecnico": 21330.00, "Cantidad_Stock": 2},
   {"Codigo": "rel002", "Producto": "RELE EMBRACO C/ BOBIN ( EN CAJA ) 1/3-1/4", "Precio_Cliente": 26000.00, "Precio_Tecnico": 22000.00, "Cantidad_Stock": 5},
-  {"Codigo": "rel003", "Producto": "RELE P/ HELADERA 4 CONTACTOS", "Precio_Cliente": 4158.00, "Precio_Tecnico": 3240.00, "Cantidad_Stock": 7},
+  {"Codigo": "rel003", "Producto": "RELE P/ HELADERA 4 CONTACTOS", "Precio_Cliente": 4800.00, "Precio_Tecnico": 3500.00, "Cantidad_Stock": 7},
   {"Codigo": "rep", "Producto": "REPARACION", "Precio_Cliente": 1.00, "Precio_Tecnico": 1.00, "Cantidad_Stock": 96},
   {"Codigo": "reshel01", "Producto": "RESISTENCIA  HEL  WHIRLPOOL WRM 39 /  W10741592", "Precio_Cliente": 42174.00, "Precio_Tecnico": 34155.00, "Cantidad_Stock": 0},
   {"Codigo": "res005", "Producto": "RESISTENCIA DREAN BLUE/NEXT C/SENSOR", "Precio_Cliente": 32769.00, "Precio_Tecnico": 30200.00, "Cantidad_Stock": 2},
