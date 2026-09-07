f5fce5680fce071f91406ef53749fd157ef66d74
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-09-07 12:37:48 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -252,7 +252,7 @@
   {"Codigo": "GAS001", "Producto": "GAS AN22 780GR. REMPLAZO 22", "Precio_Cliente": 29900.00, "Precio_Tecnico": 24800.00, "Cantidad_Stock": 5},
   {"Codigo": "g12", "Producto": "GAS ANTON YH12+", "Precio_Cliente": 22000.00, "Precio_Tecnico": 18700.00, "Cantidad_Stock": 1},
   {"Codigo": "GAS002", "Producto": "GAS MAPP X 400GR", "Precio_Cliente": 16300.00, "Precio_Tecnico": 14300.00, "Cantidad_Stock": 6},
-  {"Codigo": "GAS003", "Producto": "GAS MO49 PLUS X 750GR.", "Precio_Cliente": 34600.00, "Precio_Tecnico": 31700.00, "Cantidad_Stock": 3},
+  {"Codigo": "GAS003", "Producto": "GAS MO49 PLUS X 750GR.", "Precio_Cliente": 34600.00, "Precio_Tecnico": 32500.00, "Cantidad_Stock": 3},
   {"Codigo": "006", "Producto": "GAS NECTON R134 X900GR", "Precio_Cliente": 35000.00, "Precio_Tecnico": 29600.00, "Cantidad_Stock": 1},
   {"Codigo": "GAS006", "Producto": "GAS R134 ANTON / FREON  * 900 GRM.", "Precio_Cliente": 32300.00, "Precio_Tecnico": 29700.00, "Cantidad_Stock": 96},
   {"Codigo": "gas020", "Producto": "GAS R134 TORRINGTON", "Precio_Cliente": 27819.00, "Precio_Tecnico": 21483.00, "Cantidad_Stock": 0},
