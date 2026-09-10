a6d6409e4533c4912ff00c65db65f366460f5959
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-09-10 16:05:15 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -202,6 +202,7 @@
   {"Codigo": "do001", "Producto": "DOBLADORA DE CAÑO 3 EN 1", "Precio_Cliente": 22260.00, "Precio_Tecnico": 16960.00, "Cantidad_Stock": 0},
   {"Codigo": "res004", "Producto": "DOBLADORA RESORTE 5/8", "Precio_Cliente": 6900.00, "Precio_Tecnico": 5900.00, "Cantidad_Stock": 1},
   {"Codigo": "d2", "Producto": "DUCTO SALIDA DE AIRE PORTATIL 5\" X 2 METROS", "Precio_Cliente": 23760.00, "Precio_Tecnico": 19305.00, "Cantidad_Stock": 0},
+  {"Codigo": "ele15", "Producto": "ELECTROVALVIULA CONSUL", "Precio_Cliente": 22300.00, "Precio_Tecnico": 19700.00, "Cantidad_Stock": 1},
   {"Codigo": "emicol", "Producto": "ELECTROVALVULA  EMICOL DREAN BLUE DOBLE", "Precio_Cliente": 18400.00, "Precio_Tecnico": 16900.00, "Cantidad_Stock": 7},
   {"Codigo": "ele5", "Producto": "ELECTROVALVULA 1 VIA 180º /90º", "Precio_Cliente": 9500.00, "Precio_Tecnico": 8700.00, "Cantidad_Stock": 8},
   {"Codigo": "ele3", "Producto": "ELECTROVALVULA 2 VIAS 180º", "Precio_Cliente": 16200.00, "Precio_Tecnico": 14200.00, "Cantidad_Stock": 8},
