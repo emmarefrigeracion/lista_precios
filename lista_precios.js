c252c7032753ffc2ff56d681a812c2a14f7764af
emmarefrigeracion <emmarefrigeracionbk@gmail.com>
2026-09-16 09:35:04 -0300

    Actualización de precios

--- lista_precios.js
+++ lista_precios.js
@@ -643,6 +643,7 @@
   {"Codigo": "tur021", "Producto": "TURBINA TANGENCIAL 18CM", "Precio_Cliente": 32200.00, "Precio_Tecnico": 28900.00, "Cantidad_Stock": 2},
   {"Codigo": "tur020", "Producto": "TURBINA TANGENCIAL PARA EXHIBIDORA  24CM", "Precio_Cliente": 47000.00, "Precio_Tecnico": 39900.00, "Cantidad_Stock": 1},
   {"Codigo": "013", "Producto": "UNION BRONCE 1/2 (NIPLE)", "Precio_Cliente": 6800.00, "Precio_Tecnico": 5500.00, "Cantidad_Stock": 3},
+  {"Codigo": "012", "Producto": "UNION BRONCE 3/8 (NIPLE)", "Precio_Cliente": 5800.00, "Precio_Tecnico": 4900.00, "Cantidad_Stock": 0},
   {"Codigo": "tue012", "Producto": "UNION BRONCE 5/8 (NIPLE)", "Precio_Cliente": 7900.00, "Precio_Tecnico": 7000.00, "Cantidad_Stock": 2},
   {"Codigo": "8-6-CV12", "Producto": "VALVULA CHECK - AIRE ACONDICIONADO", "Precio_Cliente": 8246.70, "Precio_Tecnico": 7623.00, "Cantidad_Stock": 3},
   {"Codigo": "4-9-2753", "Producto": "VALVULA DE CARGA 1/4", "Precio_Cliente": 2700.00, "Precio_Tecnico": 2300.00, "Cantidad_Stock": 9},
